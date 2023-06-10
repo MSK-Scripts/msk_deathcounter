1c6839afbff4f4e9875d9fd7308182f799639679
Musiker15 <info@musiker15.de>
2023-06-10 21:41:35 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -176,7 +176,7 @@ if Config.VersionChecker then
 	    	print(resourceName .. '^2 ✓ Resource is Up to Date^0 - ^5Current Version: ^2' .. CurrentVersion .. '^0')
     	elseif CurrentVersion ~= NewestVersion then
         	print(resourceName .. '^1 ✗ Resource Outdated. Please Update!^0 - ^5Current Version: ^1' .. CurrentVersion .. '^0')
-	    	print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9https://keymaster.fivem.net/^0')
+	    	print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9https://github.com/MSK-Scripts/msk_deathcounter^0')
     	end
 		print("###############################")
 	end)
